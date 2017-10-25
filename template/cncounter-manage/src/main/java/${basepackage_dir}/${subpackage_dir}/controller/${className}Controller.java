493a20c817cfaec0684fc70cfaec92a2b98887f1
王鹏程 <wpcfree@qq.com>
2017-10-25 18:13:28 +0800

    导出模板添加页面模板和controller

--- template/cncounter-manage/src/main/java/${basepackage_dir}/${subpackage_dir}/controller/${className}Controller.java
+++ template/cncounter-manage/src/main/java/${basepackage_dir}/${subpackage_dir}/controller/${className}Controller.java
@@ -4,20 +4,19 @@
 <#assign shortName = table.shortName>
 package ${basepackage}.${subpackage}.controller;
 
-import java.util.*;
-
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
+import com.wpc.common.datatables.DataTablesRequest;
+import com.wpc.common.datatables.DataTablesResponse;
+import com.wpc.common.msg.AjaxResult;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import ${basepackage}.${subpackage}.mvc.controller.base.ControllerBase;
-import ${basepackage}.util.common.StringNumberUtil;
-import ${basepackage}.${subpackage}.mvc.msg.JSONMessage;
 import ${basepackage}.${subpackage}.model.${className};
 import ${basepackage}.${subpackage}.service.${className}Service;
 
@@ -27,100 +26,53 @@ import ${basepackage}.${subpackage}.service.${className}Service;
  */
 @Controller
 @RequestMapping("/${subpackage}/${classNameLower}")
-public class ${className}Controller extends ControllerBase {
+public class ${className}Controller {
     
     @Autowired
     private ${className}Service ${classNameLower}Service;
-    
-	@RequestMapping(value = "/list.json")
-	@ResponseBody
-	public JSONMessage list(HttpServletRequest request) {
-		// get params
-		Map<String, Object> params = parseParamMapObject(request);
-        processPageParams(params);
-		//
-		Integer count = ${classNameLower}Service.countBy(params);
-		List<${className}> ${classNameLower}List = ${classNameLower}Service.listPage(params);
-		//
-		JSONMessage jsonMessage = JSONMessage.successMessage();
-		jsonMessage.setTotal(count);
-		jsonMessage.setData(${classNameLower}List);
 
-		return jsonMessage;
+	/**
+	 * 页面跳转
+	 */
+	@RequestMapping
+	public String index(ModelMap model) {
+		return "${subpackage}/${classNameLower}";
 	}
 
-	@RequestMapping(value = "/add.json", method = RequestMethod.POST)
+	/**
+	 *
+	 * 分页查询列表
+	 */
+	@RequestMapping(value="/searchPage", method=RequestMethod.POST)
 	@ResponseBody
-	public JSONMessage doAdd(HttpServletRequest request) {
-		// get params
-		Map<String, Object> params = parseParamMapObject(request);
-		//
-		${className} ${classNameLower} = new ${className}();
-		//
-		map2Bean(params, ${classNameLower});
-		//
-		
-		//
-		JSONMessage jsonMessage = JSONMessage.failureMessage();
-        try{
-			Integer rows = ${classNameLower}Service.add(${classNameLower});
-            if(rows > 0){
-                jsonMessage = JSONMessage.successMessage();
-            }
-        } catch(Exception ex){
-            logger.error("操作失败",ex);
-        }
-		return jsonMessage;
+	public DataTablesResponse<${className}> searchPage(ModelMap model, HttpServletRequest request, @RequestBody DataTablesRequest query) {
+		return ${classNameLower}Service.searchPage(query);
 	}
-	
 
-	@RequestMapping(value = "/edit.json", method = RequestMethod.POST)
+	/**
+	 * 保存或更新
+	 */
+	@RequestMapping(value="/addOrUpdate", method=RequestMethod.POST)
 	@ResponseBody
-	public JSONMessage doEdit(HttpServletRequest request) {
-		// get params
-		Map<String, Object> params = parseParamMapObject(request);
-		//
-		${className} ${classNameLower} = new ${className}();
-		//
-		map2Bean(params, ${classNameLower});
-		//
-
-		//
-		JSONMessage jsonMessage = JSONMessage.failureMessage();
-        try{
-			Integer rows = ${classNameLower}Service.update(${classNameLower});
-            if(rows > 0){
-                jsonMessage = JSONMessage.successMessage();
-            }
-        } catch(Exception ex){
-            logger.error("操作失败",ex);
-        }
-		return jsonMessage;
-	}
-	
-
-	@RequestMapping(value = "/delete.json", method = RequestMethod.POST)
-	@ResponseBody
-	public JSONMessage delete(HttpServletRequest request) {
-		// get params
-		Map<String, Object> params = parseParamMapObject(request);
-		//
-		Integer id = 0;
-		Object _id = params.get("id");
-		if(null != _id && StringNumberUtil.isLong(_id.toString())){
-			id = StringNumberUtil.parseInt(_id.toString(), 0);
+	public AjaxResult addOrUpdate(ModelMap model, ${className} ${classNameLower}) {
+		AjaxResult ajaxResult = new AjaxResult();
+		if(${classNameLower}.getId()!=null && ${classNameLower}.getId()!=0){
+			${classNameLower}Service.update(${classNameLower});
+		}else{
+			${classNameLower}Service.save(${classNameLower});
 		}
-		//
-		JSONMessage jsonMessage = JSONMessage.failureMessage();
-        try{
-			Integer rows = ${classNameLower}Service.delete(id);
-            if(rows > 0){
-                jsonMessage = JSONMessage.successMessage();
-            }
-        } catch(Exception ex){
-            logger.error("操作失败",ex);
-        }
-		return jsonMessage;
+		return ajaxResult;
+	}
+
+	/**
+	 * 删除
+	 */
+	@RequestMapping(value="/delete", method=RequestMethod.POST)
+	@ResponseBody
+	public AjaxResult delete(ModelMap model, Long id) {
+		AjaxResult ajaxResult = new AjaxResult();
+		${classNameLower}Service.delete(id);
+		return ajaxResult;
 	}
 
 }
